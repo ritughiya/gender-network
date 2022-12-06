3758b8b88190fb14b2cfb7fcac04e16450a16cbb
ritughiya <ritughiya@gmail.com>
2022-12-06 17:16:09 -0500

    homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2af9357f97b912db"; }
+/******/ 		__webpack_require__.h = function() { return "85743684c11406d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
