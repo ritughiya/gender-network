a387ab2971d269ecb659e76f9230ef238476ceb7
ritughiya <ritughiya@gmail.com>
2023-01-04 17:49:40 -0500

    update data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3268ae1733144647"; }
+/******/ 		__webpack_require__.h = function() { return "590372bc5ccfa215"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
