2475baba99802ce52debcb5f98dfb39efb5f9388
ritughiya <ritughiya@gmail.com>
2023-01-09 19:39:42 -0500

    canvas try

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7556bcdad6f6243"; }
+/******/ 		__webpack_require__.h = function() { return "325b81fb56697025"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
