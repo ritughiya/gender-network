e6a757c2aa5d0646d8edf28421e16d896359f030
ritughiya <ritughiya@gmail.com>
2023-06-29 16:21:52 -0400

    push

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c1ae90221c30ad7"; }
+/******/ 		__webpack_require__.h = function() { return "0fa9f0d4d950837b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
