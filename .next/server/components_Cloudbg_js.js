0c5b9178031f8e890084356a8e24fa1b480e9188
ritughiya <ritughiya@gmail.com>
2023-09-27 10:47:52 -0400

    c olor

---
+++ .next/server/components_Cloudbg_js.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "components_Cloudbg_js";
+exports.ids = ["components_Cloudbg_js"];
+exports.modules = {
+
+/***/ "./components/Cloudbg.js":
+/*!*******************************!*\
+  !*** ./components/Cloudbg.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Cloudbg(props) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n            className: \"cloud\",\n            style: {\n                backgroundColor: props.color\n            },\n            children: \"            \"\n        }, void 0, false, {\n            fileName: \"/Volumes/LaCie/local/gendernetwork/web/components/Cloudbg.js\",\n            lineNumber: 4,\n            columnNumber: 10\n        }, this)\n    }, void 0, false);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Cloudbg);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9jb21wb25lbnRzL0Nsb3VkYmcuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBQSxTQUFTQSxRQUFRQyxLQUFLLEVBQUU7SUFDcEIscUJBQ0U7a0JBQ0csNEVBQUNDO1lBQ0VDLFdBQVU7WUFDVkMsT0FBTztnQkFBRUMsaUJBQWlCSixNQUFNSyxLQUFLO1lBQUM7c0JBQ3ZDOzs7Ozs7O0FBR1Q7QUFDQSxpRUFBZU4sT0FBT0EsRUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2NvbXBvbmVudHMvQ2xvdWRiZy5qcz9iM2Y2Il0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIENsb3VkYmcocHJvcHMpIHtcbiAgICByZXR1cm4gKFxuICAgICAgPD5cbiAgICAgICAgIDxkaXZcbiAgICAgICAgICAgIGNsYXNzTmFtZT1cImNsb3VkXCJcbiAgICAgICAgICAgIHN0eWxlPXt7IGJhY2tncm91bmRDb2xvcjogcHJvcHMuY29sb3IgfX1cbiAgICAgICAgICA+ICAgICAgICAgICAgPC9kaXY+XG4gIDwvPlxuICApXG4gIH1cbiAgZXhwb3J0IGRlZmF1bHQgQ2xvdWRiZyJdLCJuYW1lcyI6WyJDbG91ZGJnIiwicHJvcHMiLCJkaXYiLCJjbGFzc05hbWUiLCJzdHlsZSIsImJhY2tncm91bmRDb2xvciIsImNvbG9yIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./components/Cloudbg.js\n");
+
+/***/ })
+
+};
+;
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c539f6a50535331a"; }
+/******/ 		__webpack_require__.h = function() { return "969715c560675fd7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
